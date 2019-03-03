daced6dde61dd75ce2f262fda35e6928afd47c22
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-03-03 18:57:44 +0900

    fix zsh keybind

--- .zsh/20_keybinds.zsh
+++ .zsh/20_keybinds.zsh
@@ -4,30 +4,13 @@ bindkey -v
 bindkey -M viins 'jj' vi-cmd-mode
 
 # Add emacs-like keybind to viins mode
-bindkey -M viins '^F'  forward-char
-bindkey -M viins '^B'  backward-char
-bindkey -M viins '^A'  beginning-of-line
-bindkey -M viins '^E'  end-of-line
-bindkey -M viins '^K'  kill-line
-
-bindkey -M viins '^Y'  yank
-bindkey -M viins '^W'  backward-kill-word
-bindkey -M viins '^U'  backward-kill-line
-bindkey -M viins '^H'  backward-delete-char
-bindkey -M viins '^?'  backward-delete-char
-bindkey -M viins '^G'  send-break
-bindkey -M viins '^D'  delete-char-or-list
-
-bindkey -M vicmd '^A'  beginning-of-line
-bindkey -M vicmd '^E'  end-of-line
-bindkey -M vicmd '^K'  kill-line
-bindkey -M vicmd '^P'  up-line-or-history
-bindkey -M vicmd '^N'  down-line-or-history
-bindkey -M vicmd '^Y'  yank
+bindkey -M vicmd ' h'  beginning-of-line
+bindkey -M vicmd ' l'  end-of-line
+bindkey -M vicmd '^k'  up-line-or-history
+bindkey -M vicmd '^j'  down-line-or-history
+bindkey -M vicmd 'y'  yank
 bindkey -M vicmd '^W'  backward-kill-word
-bindkey -M vicmd '^U'  backward-kill-line
-bindkey -M vicmd '/'   vi-history-search-forward
-bindkey -M vicmd '?'   vi-history-search-backward
+bindkey -M vicmd 'q' push-line
 
 if [[ $- == *i* ]]; then
 
