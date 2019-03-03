0709fdbce2366a7068cfc0f422a1ed02bf839095
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-03-03 20:54:33 +0900

    fix yq

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -87,7 +87,7 @@ zplug "zsh-users/zsh-syntax-highlighting", \
 zplug "kislyuk/yq", \
      as:command, \
      rename-to:"yq", \
-     hook-build:"python setup.py install"
+     hook-build:"pip install yq"
 
 zplug 'dracula/zsh', as:theme
 
