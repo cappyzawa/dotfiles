094a049100080dd71d21fba01b13cbd178a4d71d
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-03-21 14:41:31 +0000

    fix indent

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -5,109 +5,105 @@ zplug "~/.zsh", from:local, use:"<->_*.zsh"
 zplug "zsh-users/zsh-completions"
 
 zplug "stedolan/jq", \
-    from:gh-r, \
-    as:command, \
-    rename-to:jq
+  from:gh-r, \
+  as:command, \
+  rename-to:jq
 
 zplug "b4b4r07/emoji-cli", \
-    on:"stedolan/jq"
+  on:"stedolan/jq"
 
 zplug "cappyzawa/get-op", \
-    as:command, \
-    hook-build:"./init", \
-    use:"op"
+  as:command, \
+  hook-build:"./init", \
+  use:"op"
 
 zplug "cappyzawa/op-kv", \
-    from:gh-r, \
-    as:command, \
-    rename-to:op-kv, \
-    on:"stedolan/jq", \
-    on:"cappyzawa/get-op"
+  from:gh-r, \
+  as:command, \
+  rename-to:op-kv, \
+  on:"stedolan/jq", \
+  on:"cappyzawa/get-op"
 
 zplug "mrowa44/emojify", as:command
 
 zplug "junegunn/fzf-bin", \
-    as:command, \
-    from:gh-r, \
-    rename-to:"fzf", \
-    frozen:1
+  as:command, \
+  from:gh-r, \
+  rename-to:"fzf", \
+  frozen:1
 
 zplug "b4b4r07/enhancd", \
-    use:init.sh, \
-    on:"junegunn/fzf-bin"
+  use:init.sh, \
+  on:"junegunn/fzf-bin"
 if zplug check "b4b4r07/enhancd"; then
-    export ENHANCD_FILTER="fzf --height 50% --reverse --ansi"
-    export ENHANCD_DOT_SHOW_FULLPATH=1
+  export ENHANCD_FILTER="fzf --height 50% --reverse --ansi"
+  export ENHANCD_DOT_SHOW_FULLPATH=1
 fi
 
 zplug "motemen/ghq", \
-    as:command, \
-    from:gh-r, \
-    rename-to:"ghq"
+  as:command, \
+  from:gh-r, \
+  rename-to:"ghq"
 
 zplug "b4b4r07/zsh-vimode-visual", \
   defer:3
 
 zplug "peco/peco", \
-    as:command, \
-    from:gh-r, \
-    frozen:1
-
-zplug "b4b4r07/zsh-gomi", \
-    as:command, \
-    use:bin/gomi
+  as:command, \
+  from:gh-r, \
+  frozen:1
 
 zplug 'b4b4r07/zplug-doctor', lazy:yes
 
 zplug "b4b4r07/ssh-keyreg", as:command, use:bin
 
 zplug "b4b4r07/stein", \
-    as:command, \
-    from:gh-r
+  as:command, \
+  from:gh-r
 
 zplug "motemen/gobump", \
-    as:command, \
-    hook-build:"go get -d && go build cmd/gobump/..."
+  as:command, \
+  hook-build:"go get -d && go build cmd/gobump/..."
 
 zplug "goreleaser/goreleaser", \
-    as:command, \
-    rename-to:"goreleaser", \
-    hook-build:"go get -d && go mod download && go build"
+  as:command, \
+  rename-to:"goreleaser", \
+  hook-build:"go get -d && go mod download && go build"
 
 zplug "b4b4r07/release-go", \
-    as:command, \
-    rename-to:"release-go", \
-    use:"./release-go.sh", \
-    on:"goreleaser/goreleaser", \
-    on:"motemen/gobump"
+  as:command, \
+  rename-to:"release-go", \
+  use:"./release-go.sh", \
+  on:"goreleaser/goreleaser", \
+  on:"motemen/gobump"
 
 zplug "zsh-users/zsh-syntax-highlighting", \
-    defer:2
+  defer:2
 
 zplug "kislyuk/yq", \
-     as:command, \
-     rename-to:"yq", \
-     hook-build:"pip install yq"
+  as:command, \
+  rename-to:"yq", \
+  hook-build:"pip install yq"
 
 zplug "adrienverge/yamllint", \
-     as:command, \
-     rename-to:"yamllint", \
-     hook-build:"python setup.py install"
+  as:command, \
+  rename-to:"yamllint", \
+  hook-build:"python setup.py install"
 
 zplug "github/hub", \
-     as:command, \
-     rename-to:"hub", \
-     hook-build:"go get -d && make install"
+  as:command, \
+  rename-to:"hub", \
+  hook-build:"go get -d && make install"
 
 zplug "hairyhenderson/gomplate", \
-     as:command, \
-     from:"gh-r", \
-     rename-to:"gomplate"
+  as:command, \
+  from:"gh-r", \
+  rename-to:"gomplate"
 
 zplug "get-ytt/ytt", \
-     as:command, \
-     from:"gh-r", \
-     rename-to:"ytt"
+  as:command, \
+  from:"gh-r", \
+  rename-to:"ytt"
 
 zplug 'dracula/zsh', as:theme
 
