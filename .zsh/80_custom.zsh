d3c0f3ab26cc378c6b5c56183b9c12f3d3f8fa58
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-04-01 19:25:43 +0900

    add 9k settings

--- .zsh/80_custom.zsh
+++ .zsh/80_custom.zsh
@@ -1,6 +1,14 @@
 if zplug_installed bhilburn powerlevel9k; then
+  # for prompt
   export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon vi_mode dir vcs)
   export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time battery)
+
+  # for battery
   export POWERLEVEL9K_BATTERY_VERBOSE=false
   export POWERLEVEL9K_BATTERY_STAGES=($'\u2581 ' $'\u2582 ' $'\u2583 ' $'\u2584 ' $'\u2585 ' $'\u2586 ' $'\u2587 ' $'\u2588 ')
+
+  # for dir
+  export POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+  export POWERLEVEL9K_SHORTEN_DELIMITER="…"
+  export POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
 fi
