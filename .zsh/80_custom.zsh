3ac27e352ecef093238788c3d3162ae8ffcf3e25
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-07-16 16:23:02 +0900

    remove iterm2

--- .zsh/80_custom.zsh
+++ .zsh/80_custom.zsh
@@ -4,17 +4,8 @@ if zplug_installed bhilburn powerlevel9k; then
     PROMPT='$ '
   else
     # for prompt
-    export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon vi_mode dir vcs)
-    export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time battery)
-
-    # for battery
-    export POWERLEVEL9K_BATTERY_VERBOSE=false
-    export POWERLEVEL9K_BATTERY_STAGES=($'\u2581 ' $'\u2582 ' $'\u2583 ' $'\u2584 ' $'\u2585 ' $'\u2586 ' $'\u2587 ' $'\u2588 ')
-
-    # for dir
-    export POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
-    export POWERLEVEL9K_SHORTEN_DELIMITER="…"
-    export POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
+    export POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode kubecontext)
+    export POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
   fi
 fi
 
