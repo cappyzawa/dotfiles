87fd82d9281c2a60199fafef7374ff6c6d8619cd
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-08-23 11:40:29 +0900

    add npm global install

---
+++ .zsh/64_node.zsh
@@ -0,0 +1,23 @@
+npm_global_install(){
+  pkg=$1
+  echo "dotfile: Install ${pkg}"
+  npm install -g ${pkg}
+}
+
+if has "npm"; then
+  if ! has "elm"; then
+    npm_global_install elm
+  fi
+
+  if ! has "elm-test"; then
+    npm_global_install "elm-test"
+  fi
+
+  if ! has "elm-format"; then
+    npm_global_install "elm-format"
+  fi
+
+  if ! has "elm-language-server"; then
+    npm_global_install "@elm-tooling/elm-language-server"
+  fi
+fi
