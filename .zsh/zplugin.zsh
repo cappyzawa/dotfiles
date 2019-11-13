abb774fed4bd50bc7c0204ca3a47915b30fb2f61
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-11-13 14:46:21 +0900

    add minio/mc

--- .zsh/zplugin.zsh
+++ .zsh/zplugin.zsh
@@ -146,6 +146,10 @@ zplugin ice wait'3' lucid as"program" has"go" \
   atclone"go install . && kind completion zsh > ~/.zsh/Completion/_kind" atpull"%atclone"
 zplugin light kubernetes-sigs/kind
 
+zplugin ice wait'3' lucid as"program" has"go" \
+  atclone"GO111MODULE=on go install ." atpull"%atclone"
+zplugin light minio/mc
+
 zplugin ice wait'2' lucid as"program" from:"gh-r" pick:"tkn" \
   atclone"./tkn completion zsh > ~/.zsh/Completion/_tkn" atpull"%atclone"
 zplugin light tektoncd/cli
