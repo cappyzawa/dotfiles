96d58dcb3037663c1892c5086be8edcd39ff501b
cappyzawa <cappyzawa@yahoo.ne.jp>
2020-05-26 12:41:40 +0900

    remove tmux from zinit

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -2,7 +2,7 @@
 zinit light zinit-zsh/z-a-rust
 # }}}
 
-zinit ice from"gh-r" as"program" lucid
+zinit ice lucid from"gh-r" as"program"
 zinit light "junegunn/fzf-bin"
 
 zinit ice pick"init.sh" lucid \
