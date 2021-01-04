8a62ae9474d63fc4a0d8db7312d805d40b2ef26d
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2021-01-05 01:59:40 +0900

    use https://github.com/glepnir/galaxyline.nvim/blob/a1f1d6701af80af4627eb292d8893a68ff0b4a82/example/eviline.lua

--- .config/nvim/lua/gl.lua
+++ .config/nvim/lua/gl.lua
@@ -19,8 +19,8 @@ local colors = {
   white = '#acb0d0',
 }
 
-local empty_buffer = function()
-  if vim.fn.empty(vim.fn.expand('%:t')) < 1 then
+local buffer_not_empty = function()
+  if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then
     return true
   end
   return false
@@ -28,54 +28,52 @@ end
 
 gls.left[1] = {
   FirstElement = {
-    provider = function() return '  ' end,
-    highlight = {colors.grey,colors.yellow}
+    provider = function() return '▊ ' end,
+    highlight = {colors.blue,colors.line_bg}
   },
 }
 gls.left[2] = {
   ViMode = {
     provider = function()
-      local alias = {n = 'NORMAL ',i = 'INSERT ',c= 'COMMAND ',V= 'VISUAL', [''] = 'VISUAL '}
-      return alias[vim.fn.mode()]
+      -- auto change color according the vim mode
+      local mode_color = {n = colors.magenta, i = colors.green,v=colors.blue,[''] = colors.blue,V=colors.blue,
+                          c = colors.red,no = colors.magenta,s = colors.orange,S=colors.orange,
+                          [''] = colors.orange,ic = colors.yellow,R = colors.purple,Rv = colors.purple,
+                          cv = colors.red,ce=colors.red, r = colors.cyan,rm = colors.cyan, ['r?'] = colors.cyan,
+                          ['!']  = colors.red,t = colors.red}
+      vim.api.nvim_command('hi GalaxyViMode guifg='..mode_color[vim.fn.mode()])
+      return '  '
     end,
-    separator = ' ',
-    separator_highlight = {colors.yellow,function()
-      if not empty_buffer() then
-        return colors.purple
-      end
-      return colors.darkblue
-    end},
-    highlight = {colors.grey,colors.yellow,'bold'},
+    highlight = {colors.red,colors.line_bg,'bold'},
   },
 }
 gls.left[3] ={
   FileIcon = {
     provider = 'FileIcon',
-    condition = empty_buffer,
-    highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,colors.darkblue},
+    condition = buffer_not_empty,
+    highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,colors.line_bg},
   },
 }
 gls.left[4] = {
   FileName = {
     provider = {'FileName','FileSize'},
-    condition = empty_buffer,
-    separator = ' ',
-    separator_highlight = {colors.purple,colors.darkblue},
-    highlight = {colors.white,colors.darkblue}
+    condition = buffer_not_empty,
+    highlight = {colors.fg,colors.line_bg,'bold'}
   }
 }
+
 gls.left[5] = {
   GitIcon = {
-    provider = function() return '  ' end,
-    condition = empty_buffer,
-    highlight = {colors.orange,colors.purple},
+    provider = function() return '  ' end,
+    condition = require('galaxyline.provider_vcs').check_git_workspace,
+    highlight = {colors.orange,colors.line_bg},
   }
 }
 gls.left[6] = {
   GitBranch = {
     provider = 'GitBranch',
-    condition = empty_buffer,
-    highlight = {colors.grey,colors.purple},
+    condition = require('galaxyline.provider_vcs').check_git_workspace,
+    highlight = {'#8FBCBB',colors.line_bg,'bold'},
   }
 }
 
@@ -91,8 +89,8 @@ gls.left[7] = {
   DiffAdd = {
     provider = 'DiffAdd',
     condition = checkwidth,
-    icon = '⇡ ',
-    highlight = {colors.green,colors.purple},
+    icon = ' ',
+    highlight = {colors.green,colors.line_bg},
   }
 }
 gls.left[8] = {
@@ -100,29 +98,29 @@ gls.left[8] = {
     provider = 'DiffModified',
     condition = checkwidth,
     icon = ' ',
-    highlight = {colors.orange,colors.purple},
+    highlight = {colors.orange,colors.line_bg},
   }
 }
 gls.left[9] = {
   DiffRemove = {
     provider = 'DiffRemove',
     condition = checkwidth,
-    icon = '⇣ ',
-    highlight = {colors.red,colors.purple},
+    icon = ' ',
+    highlight = {colors.red,colors.line_bg},
   }
 }
 gls.left[10] = {
   LeftEnd = {
-    provider = function() return ' | ' end,
-    separator = ' ',
-    separator_highlight = {colors.purple,colors.bg},
-    highlight = {colors.purple,colors.purple}
+    provider = function() return '' end,
+    separator = '',
+    separator_highlight = {colors.bg,colors.line_bg},
+    highlight = {colors.line_bg,colors.line_bg}
   }
 }
 gls.left[11] = {
   DiagnosticError = {
     provider = 'DiagnosticError',
-    icon = '  ',
+    icon = '  ',
     highlight = {colors.red,colors.bg}
   }
 }
@@ -135,54 +133,55 @@ gls.left[13] = {
   DiagnosticWarn = {
     provider = 'DiagnosticWarn',
     icon = '  ',
-    highlight = {colors.yellow,colors.bg},
+    highlight = {colors.blue,colors.bg},
   }
 }
 gls.right[1]= {
   FileFormat = {
     provider = 'FileFormat',
-    separator = ' ',
-    separator_highlight = {colors.bg,colors.purple},
-    highlight = {colors.grey,colors.purple},
+    separator = ' ',
+    separator_highlight = {colors.bg,colors.line_bg},
+    highlight = {colors.fg,colors.line_bg,'bold'},
   }
 }
 gls.right[2] = {
   LineInfo = {
     provider = 'LineColumn',
     separator = ' | ',
-    separator_highlight = {colors.darkblue,colors.purple},
-    highlight = {colors.grey,colors.purple},
+    separator_highlight = {colors.blue,colors.line_bg},
+    highlight = {colors.fg,colors.line_bg},
   },
 }
 gls.right[3] = {
   PerCent = {
     provider = 'LinePercent',
     separator = ' ',
-    separator_highlight = {colors.darkblue,colors.purple},
-    highlight = {colors.white,colors.darkblue},
+    separator_highlight = {colors.line_bg,colors.line_bg},
+    highlight = {colors.cyan,colors.darkblue,'bold'},
   }
 }
 gls.right[4] = {
   ScrollBar = {
     provider = 'ScrollBar',
-    highlight = {colors.yellow,colors.purple},
+    highlight = {colors.blue,colors.purple},
   }
 }
 
 gls.short_line_left[1] = {
   BufferType = {
     provider = 'FileTypeName',
-    separator = ' ',
+    separator = '',
     separator_highlight = {colors.purple,colors.bg},
-    highlight = {colors.grey,colors.purple}
+    highlight = {colors.fg,colors.purple}
   }
 }
+
 
 gls.short_line_right[1] = {
   BufferIcon = {
     provider= 'BufferIcon',
-    separator = ' ',
+    separator = '',
     separator_highlight = {colors.purple,colors.bg},
-    highlight = {colors.grey,colors.purple}
+    highlight = {colors.fg,colors.purple}
   }
 }
