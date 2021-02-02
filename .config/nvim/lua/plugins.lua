232e97515520f8ac6f168fec22d7b2b851a6213d
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2021-02-02 22:58:48 +0900

    use nvim-autopairs

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -105,9 +105,30 @@ return require'packer'.startup(function()
     keys = {'gc'}
   }
   use {
-    'jiangmiao/auto-pairs',
+    'windwp/nvim-autopairs',
     config = function()
-      vim.g.AutoPairsMapCR = 0
+      local remap = vim.api.nvim_set_keymap
+      local npairs = require('nvim-autopairs')
+
+      -- skip it, if you use another global object
+      _G.MUtils= {}
+
+      vim.g.completion_confirm_key = ""
+      MUtils.completion_confirm=function()
+        if vim.fn.pumvisible() ~= 0  then
+          if vim.fn.complete_info()["selected"] ~= -1 then
+            vim.fn["compe#confirm"]()
+            return npairs.esc("<c-y>")
+          else
+            vim.fn.nvim_select_popupmenu_item(0, false, false,{})
+            vim.fn["compe#confirm"]()
+            return npairs.esc("<c-n><c-y>")
+          end
+        else
+          return npairs.check_break_line_char()
+        end
+        remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
+      end
     end
   }
   use {
