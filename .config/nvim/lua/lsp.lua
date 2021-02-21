2c91288605cacd79b94113cec9881bc073b794f7
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2021-02-21 14:39:51 +0900

    update

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -131,34 +131,38 @@ lspconfig.terraformls.setup{
 }
 
 --go
-function Goimports(timeoutms)
+-- https://github.com/golang/tools/pull/276
+function goimports(timeoutms)
   local context = { source = { organizeImports = true } }
   vim.validate { context = { context, "t", true } }
+
   local params = vim.lsp.util.make_range_params()
   params.context = context
-  local method = "textDocument/codeAction"
-  local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
-  if resp and resp[1] then
-    local result = resp[1].result
-    if result and result[1] then
-      local edit = result[1].edit
-      vim.lsp.util.apply_workspace_edit(edit)
+
+  -- See the implementation of the textDocument/codeAction callback
+  -- (lua/vim/lsp/handler.lua) for how to do this properly.
+  local result = lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+  if not result or next(result) == nil then return end
+  local actions = result[1].result
+  if not actions then return end
+  local action = actions[1]
+
+  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+  -- is a CodeAction, it can have either an edit, a command or both. Edits
+  -- should be executed first.
+  if action.edit or type(action.command) == "table" then
+    if action.edit then
+      vim.lsp.util.apply_workspace_edit(action.edit)
     end
+    if type(action.command) == "table" then
+      vim.lsp.buf.execute_command(action.command)
+    end
+  else
+    vim.lsp.buf.execute_command(action)
   end
-  vim.lsp.buf.formatting()
 end
-lspconfig.gopls.setup{
-  cmd = {"gopls", "serve"},
-  settings = {
-      gopls = {
-          analyses = {
-              unusedparams = true,
-          },
-          staticcheck = true,
-      }
-  }
-}
-vim.cmd([[ autocmd BufWritePost *.go lua Goimports(1000) ]])
+
+vim.cmd([[ autocmd BufWritePost *.go lua goimports(1000) ]])
 
 --rust
 lspconfig.rust_analyzer.setup{
