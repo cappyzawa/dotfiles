c44a9b8638b39133a6d67cd45b4f2a6808e7bb59
Shu Kutsuzawa <cappyzawa@gmail.com>
2021-07-20 22:19:44 +0900

    remove hop.nvim & use lightspeed.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -97,6 +97,7 @@ return require'packer'.startup(function()
 
       -- hcl {{{
       execute('autocmd BufRead,BufNewFile *.hcl set filetype=hcl')
+      -- }}}
     end
   }
   use {
@@ -220,9 +221,28 @@ return require'packer'.startup(function()
     keys = {'<C-e>'}
   }
   use {
-    'phaazon/hop.nvim',
+    'ggandor/lightspeed.nvim',
     config = function()
-      vim.api.nvim_set_keymap('n', '$', "<cmd>lua require'hop'.hint_words()<cr>", {})
+      require'lightspeed'.setup {
+        jump_to_first_match = true,
+        jump_on_partial_input_safety_timeout = 400,
+        -- This can get _really_ slow if the window has a lot of content,
+        -- turn it on only if your machine can always cope with it.
+        highlight_unique_chars = false,
+        grey_out_search_area = true,
+        match_only_the_start_of_same_char_seqs = true,
+        limit_ft_matches = 5,
+        full_inclusive_prefix_key = '<c-x>',
+        -- By default, the values of these will be decided at runtime,
+        -- based on `jump_to_first_match`.
+        labels = nil,
+        cycle_group_fwd_key = nil,
+        cycle_group_bwd_key = nil,
+      }
+      local lightspeed_sc = {'f', 'F', 't', 'T'}
+      for _, v in pairs(lightspeed_sc) do
+        vim.api.nvim_set_keymap('n', v, string.format('reg_recording() . reg_executing() == "" ? "<Plug>Lightspeed_%s" : "%s"', v, v), {expr = true})
+      end
     end,
   }
   use {
