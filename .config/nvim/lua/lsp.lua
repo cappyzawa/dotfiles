546a63edea75532cee7c6d8442a41f14c3991fd1
Shu Kutsuzawa <cappyzawa@gmail.com>
2022-02-03 02:16:56 +0900

    fix julia lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -267,27 +267,6 @@ local clang_lsp_config = {cmd = {clangd_binary, "--background-index"}}
 -- }}}
 
 -- julia {{{
-lspconfig.julials.setup {
-    on_attach = on_attach,
-    -- capabilities = capabilities,
-    on_new_config = function(new_config, new_root_dir)
-        cmd = {
-            "julia", "--startup-file=no", "--history-file=no", "-e", [[
-          using Pkg;
-          Pkg.instantiate()
-          using LanguageServer; using SymbolServer;
-          depot_path = get(ENV, "JULIA_DEPOT_PATH", "")
-          project_path = dirname(something(Base.current_project(pwd()), Base.load_path_expand(LOAD_PATH[2])))
-          # Make sure that we only load packages from this environment specifically.
-          @info "Running language server" env=Base.load_path()[1] pwd() project_path depot_path
-          server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path, depot_path);
-          server.runlinter = true;
-          run(server);
-        ]]
-        };
-        new_config.cmd = cmd
-    end
-}
 -- }}}
 
 -- elm {{{
@@ -339,6 +318,7 @@ local servers = {
     pylsp = python_lsp_config,
     clangd = clang_lsp_config,
     solargraph = {},
+    julials = {},
     efm = efm_lsp_config
 }
 
