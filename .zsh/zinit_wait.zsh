292aa3ab2b033798433c622cbe5b979abc6072ee
Shu Kutsuzawa <cappyzawa@gmail.com>
2022-12-17 12:06:19 +0900

    execute shellfmt

--- .zsh/zinit_wait.zsh
+++ .zsh/zinit_wait.zsh
@@ -14,51 +14,51 @@ zinit snippet $HOME/.zsh/80_custom.zsh
 # utils {{{
 # neovim/neovim {{{
 zinit ice wait"1" lucid as"program" pick"nvim*/bin/nvim" from:"gh-r" \
-  if'is_mac' \
-  bpick"nvim-macos.tar.gz" ver"nightly"
+	if'is_mac' \
+	bpick"nvim-macos.tar.gz" ver"nightly"
 zinit light neovim/neovim
 # }}}
 # b4b4r07/enhancd {{{
 zinit ice wait pick"init.sh" lucid \
-  atload'export ENHANCD_FILTER="fzf --height 50% --reverse --ansi";export ENHANCD_DOT_SHOW_FULLPATH=1' \
-  atclone"zinit cclear" atpull"%atclone"
+	atload'export ENHANCD_FILTER="fzf --height 50% --reverse --ansi";export ENHANCD_DOT_SHOW_FULLPATH=1' \
+	atclone"zinit cclear" atpull"%atclone"
 zinit light "b4b4r07/enhancd"
 # }}}
 # tree-sitter/tree-sitter {{{
 zinit ice wait lucid as"program" from"gh-r" \
-  mv"tree-sitter-* -> tree-sitter"
+	mv"tree-sitter-* -> tree-sitter"
 zinit light tree-sitter/tree-sitter
 # }}}
 # starship/starship {{{
 if is_mac; then
-zinit ice wait lucid as"command" from"gh-r" \
-  bpick'starship-x86_64-apple-darwin.tar.gz' \
-  atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
-  atpull"%atclone" src"init.zsh" \
-  atload"export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml"
-zinit light starship/starship
+	zinit ice wait lucid as"command" from"gh-r" \
+		bpick'starship-x86_64-apple-darwin.tar.gz' \
+		atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+		atpull"%atclone" src"init.zsh" \
+		atload"export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml"
+	zinit light starship/starship
 fi
 # }}}
 # ogham/exa (as ls) {{{
 zinit ice wait lucid as"program" from"gh-r" \
-  pick"bin/exa" \
-  atload="alias ls='exa'"
+	pick"bin/exa" \
+	atload="alias ls='exa'"
 zinit light ogham/exa
 # }}}
 # BurntSushi/ripgrep {{{
 zinit ice wait lucid as"program" from"gh-r" \
-  pick"ripgrep-*/rg"
+	pick"ripgrep-*/rg"
 zinit light BurntSushi/ripgrep
 # }}}
 # stedolan/jq {{{
 zinit ice wait"1" as"program" from"gh-r" lucid \
-  mv"jq-* -> jq" pick"jq" \
-  atload"alias -g JQ='| jq -C .' && alias -g JL='| jq -C . | less -R -X'"
+	mv"jq-* -> jq" pick"jq" \
+	atload"alias -g JQ='| jq -C .' && alias -g JL='| jq -C . | less -R -X'"
 zinit light stedolan/jq
 # }}}
 # jonas/tig {{{
 zinit ice wait"1" as"program" lucid \
-  make"install prefix=$ZPFX" pick"$ZPFX/bin/tig"
+	make"install prefix=$ZPFX" pick"$ZPFX/bin/tig"
 zinit light jonas/tig
 # }}}
 # jesseduffield/lazygit {{{
@@ -79,43 +79,43 @@ zinit light cjbassi/gotop
 # }}}
 # Rasukarusan/fzf-chrome-active-tab (as tl) {{{
 zinit ice lucid wait"2" as"program" from"gh-r" \
-  has"fzf" mv"chrome-tab-activate->tl"
+	has"fzf" mv"chrome-tab-activate->tl"
 zinit light Rasukarusan/fzf-chrome-active-tab
 # }}}
 # mikefarah/yq {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  mv"yq* -> yq"
+	mv"yq* -> yq"
 zinit light mikefarah/yq
 # }}}
 # cli/cli (as gh) {{{
 zinit ice wait'1' lucid as"program" from:"gh-r" \
-  mv"gh*/bin/gh->gh" \
-  atclone"gh completion --shell zsh > ~/.zsh/Completion/_gh" \
-  atpull"%atclone"
+	mv"gh*/bin/gh->gh" \
+	atclone"gh completion --shell zsh > ~/.zsh/Completion/_gh" \
+	atpull"%atclone"
 zinit light "cli/cli"
 # }}}
 # sharkdp/bat {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  pick"bat-v*/bat" \
-  atload"compdef _gnu_generic bat"
+	pick"bat-v*/bat" \
+	atload"compdef _gnu_generic bat"
 zinit light sharkdp/bat
 # }}}
 # sharkdp/fd {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  pick"fd-*/fd" \
-  atload"compdef _gnu_generic fd"
+	pick"fd-*/fd" \
+	atload"compdef _gnu_generic fd"
 zinit light sharkdp/fd
 # }}}
 # sharkdp/diskus {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  pick"diskus-*/diskus" \
-  atload"compdef _gnu_generic diskus"
+	pick"diskus-*/diskus" \
+	atload"compdef _gnu_generic diskus"
 zinit light sharkdp/diskus
 # }}}
 # dandavison/delta {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  pick"delta-*/delta" \
-  atload"compdef _gnu_generic delta"
+	pick"delta-*/delta" \
+	atload"compdef _gnu_generic delta"
 zinit light dandavison/delta
 # }}}
 # o2sh/onefetch {{{
@@ -124,7 +124,7 @@ zinit light o2sh/onefetch
 # }}}
 # google/starlark-go {{{
 zinit ice wait'3' lucid as"program" has"go" \
-  atclone"go install ./cmd/starlark/..." atpull"%atclone"
+	atclone"go install ./cmd/starlark/..." atpull"%atclone"
 zinit light google/starlark-go
 # }}}
 # open-policy-agent/conftest {{{
@@ -133,18 +133,18 @@ zinit light open-policy-agent/conftest
 # }}}
 # open-policy-agent/opa {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  mv"opa* -> opa"
+	mv"opa* -> opa"
 zinit light open-policy-agent/opa
 # }}}
 # smallstep/cli {{{
 zinit ice wait'3' lucid as"program" from"gh-r" \
-  mv"step_*/bin/step -> $ZPFX/bin/step" \
-  atclone"$ZPFX/bin/step completion zsh > ~/.zsh/Completion/_step" atpull"%atclone"
+	mv"step_*/bin/step -> $ZPFX/bin/step" \
+	atclone"$ZPFX/bin/step completion zsh > ~/.zsh/Completion/_step" atpull"%atclone"
 zinit light smallstep/cli
 # }}}
 # smallstep/certificates {{{
 zinit ice wait'3' lucid as"program" from"gh-r" \
-  pick"step-ca*/bin/step-ca"
+	pick"step-ca*/bin/step-ca"
 zinit light smallstep/certificates
 # }}}
 
@@ -154,21 +154,21 @@ zinit light mike-engel/jwt-cli
 # }}}
 # cheat/cheat {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  mv"cheat-*->cheat"
+	mv"cheat-*->cheat"
 zinit light cheat/cheat
 # }}}
 # Aloxaf/fzf-tab {{{
 zinit ice wait'2' lucid as"program" \
-  atload"source ./fzf-tab.plugin.zsh"
+	atload"source ./fzf-tab.plugin.zsh"
 zinit light Aloxaf/fzf-tab
 # }}}
 # alacritty/alacritty {{{
 zinit ice wait'2' lucid as"program" \
-  if'[[ "$(ostype)" == "darwin" ]]' \
-  make!"app" \
-  atclone"cp -r target/release/${PLATFORM}/Alacritty.app /Applications" \
-  atpull"%atclone" \
-  has"cargo"
+	if'[[ "$(ostype)" == "darwin" ]]' \
+	make!"app" \
+	atclone"cp -r target/release/${PLATFORM}/Alacritty.app /Applications" \
+	atpull"%atclone" \
+	has"cargo"
 zinit light alacritty/alacritty
 # }}}
 # }}}
@@ -176,7 +176,7 @@ zinit light alacritty/alacritty
 # docker {{{
 # jesseduffield/lazydocker (as ld) {{{
 zinit ice wait"1" as"program" from"gh-r" pick"lazydocker" lucid \
-  atload"alias ld='lazydocker'"
+	atload"alias ld='lazydocker'"
 zinit light jesseduffield/lazydocker
 # }}}
 # wagoodman/dive {{{
@@ -185,7 +185,7 @@ zinit light wagoodman/dive
 # }}}
 # dagger/dagger {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  atclone"./dagger completion zsh > ~/.zsh/Completion/_dagger" atpull"%atclone"
+	atclone"./dagger completion zsh > ~/.zsh/Completion/_dagger" atpull"%atclone"
 zinit light dagger/dagger
 # }}}
 # }}}
@@ -193,28 +193,28 @@ zinit light dagger/dagger
 # golang {{{
 # goreleaser/goreleaser {{{
 zinit ice wait"3" as"program" from"gh-r" lucid \
-  pick"goreleaser" has"go"
+	pick"goreleaser" has"go"
 zinit light goreleaser/goreleaser
 # }}}
 # golangci/golangci-lint {{{
 zinit ice wait"3" as"program" from"gh-r" pick"golangci-lint-*/golangci-lint" lucid \
-  has"go" \
-  atclone"golangci-lint completion zsh > ~/.zsh/Completion/_golangci-lint"
+	has"go" \
+	atclone"golangci-lint completion zsh > ~/.zsh/Completion/_golangci-lint"
 zinit light golangci/golangci-lint
 # }}}
 # dominikh/go-tools (id-as"golang/staticcheck") {{{
 zinit ice wait"1" lucid as"program" from"gh-r" id-as"golang/staticcheck" \
-  pick"staticcheck/staticcheck"
+	pick"staticcheck/staticcheck"
 zinit light dominikh/go-tools
 # }}}
 # go-delve/delve {{{
 zinit ice wait'3' lucid as"program" pick"$ZPFX/bin/dlv" has"go" \
-  atclone"go build -o $ZPFX/bin/dlv cmd/dlv/main.go" atpull"%atclone"
+	atclone"go build -o $ZPFX/bin/dlv cmd/dlv/main.go" atpull"%atclone"
 zinit light go-delve/delve
 # }}}
 # golang/dep {{{
 zinit ice wait'3' lucid as"program" has"go" \
-  atclone"./install.sh" atpull"%atclone"
+	atclone"./install.sh" atpull"%atclone"
 zinit light golang/dep
 # }}}
 # }}}
@@ -222,7 +222,7 @@ zinit light golang/dep
 # rust {{{
 # rust-analyzer/rust-analyzer {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  mv"rust-analyzer* -> rust-analyzer" has:"rustup"
+	mv"rust-analyzer* -> rust-analyzer" has:"rustup"
 zinit light rust-lang/rust-analyzer
 # }}}
 # }}}
@@ -230,7 +230,7 @@ zinit light rust-lang/rust-analyzer
 # lua {{{
 # Koihik/vscode-lua-format {{{
 zinit ice wait'2' lucid as"program" \
-  pick"bin/$(ostype)/lua-format"
+	pick"bin/$(ostype)/lua-format"
 zinit light Koihik/vscode-lua-format
 # }}}
 # }}}
@@ -242,7 +242,7 @@ zinit light adrienverge/yamllint
 # }}}
 # vmware-tanzu/carvel-ytt {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  mv"ytt-* -> ytt" pick"ytt"
+	mv"ytt-* -> ytt" pick"ytt"
 zinit light vmware-tanzu/carvel-ytt
 # }}}
 # google/yamlfmt {{{
@@ -253,18 +253,18 @@ zinit light google/yamlfmt
 
 # zsh {{{
 zinit wait lucid for \
- atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
-    zdharma-continuum/fast-syntax-highlighting \
- atload"!_zsh_autosuggest_start" \
-    zsh-users/zsh-autosuggestions \
- blockf \
-    zsh-users/zsh-completions
+	atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
+	zdharma-continuum/fast-syntax-highlighting \
+	atload"!_zsh_autosuggest_start" \
+	zsh-users/zsh-autosuggestions \
+	blockf \
+	zsh-users/zsh-completions
 # }}}
 
 # zig {{{
 # zigtools/zls {{{
 zinit ice wait'2' lucid as"program" from"gh-r" has"zig" \
-  pick"bin/zls"
+	pick"bin/zls"
 zinit light zigtools/zls
 # }}}
 # }}}
@@ -272,12 +272,12 @@ zinit light zigtools/zls
 # shell {{{
 # koalaman/shellcheck {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  pick"shellcheck-*/shellcheck"
+	pick"shellcheck-*/shellcheck"
 zinit light koalaman/shellcheck
 # }}}
 # mvdan/sh {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  mv"shfmt_* -> shfmt"
+	mv"shfmt_* -> shfmt"
 zinit light mvdan/sh
 # }}}
 # }}}
@@ -296,7 +296,7 @@ zinit light github/hub
 # kubernetes {{{
 # GoogleContainerTools/kpt {{{
 zinit ice wait'3' lucid as"program" has"go" \
-  atclone"GO111MODULE=on go install ./..."
+	atclone"GO111MODULE=on go install ./..."
 zinit light GoogleContainerTools/kpt
 # }}}
 # zlabjp/kubernetes-scripts (as create-kubeconfig) {{{
@@ -305,14 +305,14 @@ zinit light zlabjp/kubernetes-scripts
 # }}}
 # kubernetes-sigs/kustomize {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  mv"kustomize* -> kustomize" \
-  atclone"./kustomize completion zsh > ~/.zsh/Completion/_kustomize" \
-  atpull"%atclone"
+	mv"kustomize* -> kustomize" \
+	atclone"./kustomize completion zsh > ~/.zsh/Completion/_kustomize" \
+	atpull"%atclone"
 zinit light kubernetes-sigs/kustomize
 # }}}
 # wercker/stern {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  mv"stern* -> stern"
+	mv"stern* -> stern"
 zinit light wercker/stern
 # }}}
 # ahmetb/kubectx {{{
@@ -321,63 +321,62 @@ zinit light ahmetb/kubectx
 # }}}
 # google/ko {{{
 zinit ice wait'3' lucid as"program" from"gh-r" \
-  atclone"ko completion --zsh > ~/.zsh/Completion/_ko" atpull"%atclone" \
-  atload"export KO_DOCKER_REPO='docker.io/cappyzawa'"
+	atclone"ko completion --zsh > ~/.zsh/Completion/_ko" atpull"%atclone" \
+	atload"export KO_DOCKER_REPO='docker.io/cappyzawa'"
 zinit light google/ko
 # }}}
 # operator-framework/operator-sdk {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  bpick"operator-sdk_*" \
-  mv"operator-sdk_*->operator-sdk"
+	bpick"operator-sdk_*" \
+	mv"operator-sdk_*->operator-sdk"
 zinit light operator-framework/operator-sdk
 # }}}
 # kubernetes-sigs/kubebuilder {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  mv"kubebuilder_*->kubebuilder" \
-  atclone"kubebuilder completion zsh > ~/.zsh/Completion/_kubebuilder" \
-  atpull"%atclone"
+	mv"kubebuilder_*->kubebuilder" \
+	atclone"kubebuilder completion zsh > ~/.zsh/Completion/_kubebuilder" \
+	atpull"%atclone"
 zinit light kubernetes-sigs/kubebuilder
 # }}}
 # kubernetes-sigs/kind {{{
 zinit ice wait'3' lucid as"program" from:"gh-r" \
-  mv"kind-*->kind" \
-  atclone"kind completion zsh > ~/.zsh/Completion/_kind" atpull"%atclone"
+	mv"kind-*->kind" \
+	atclone"kind completion zsh > ~/.zsh/Completion/_kind" atpull"%atclone"
 zinit light kubernetes-sigs/kind
 # }}}
 # tektoncd/cli {{{
 zinit ice wait'2' lucid as"program" from"gh-r" pick"tkn" \
-  atclone"./tkn completion zsh > ~/.zsh/Completion/_tkn && ln -s `which tkn` /usr/local/bin/kubectl-tkn" atpull"%atclone"
+	atclone"./tkn completion zsh > ~/.zsh/Completion/_tkn && ln -s $(which tkn) /usr/local/bin/kubectl-tkn" atpull"%atclone"
 zinit light tektoncd/cli
 # }}}
 # okteto/okteto {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  mv"okteto-*->okteto" \
-  atload"export KUBECONFIG=$HOME/.kube/config:$HOME/.config/okteto/okteto-kube.config"
+	mv"okteto-*->okteto"
 zinit light okteto/okteto
 # }}}
 # kubernetes-sigs/controller-runtime#setup-envtest {{{
 zinit ice wait'1' lucid as"program" \
-  id-as"kubernetes-sigs/setup-envtest" \
-  atclone"pushd tools/setup-envtest; go install; popd" \
-  atpull"%atclone"
+	id-as"kubernetes-sigs/setup-envtest" \
+	atclone"pushd tools/setup-envtest; go install; popd" \
+	atpull"%atclone"
 zinit light kubernetes-sigs/controller-runtime
 # }}}
 # sh0rez/kubectl-neat-diff {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  mv"kubectl-neat-diff-* -> kubectl-neat-diff" \
-  atload"export KUBECTL_EXTERNAL_DIFF=kubectl-neat-diff"
+	mv"kubectl-neat-diff-* -> kubectl-neat-diff" \
+	atload"export KUBECTL_EXTERNAL_DIFF=kubectl-neat-diff"
 zinit light sh0rez/kubectl-neat-diff
 # }}}
 # kubernetes-sigs/krew {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  mv"krew-*->krew" \
-  atclone"./krew install krew && ./krew completion zsh > ~/.zsh/Completion/_krew" atpull"%atclone"
+	mv"krew-*->krew" \
+	atclone"./krew install krew && ./krew completion zsh > ~/.zsh/Completion/_krew" atpull"%atclone"
 zinit light kubernetes-sigs/krew
 # }}}
 # FairwindsOps/pluto {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  atclone"./pluto completion zsh > ${HOME}/.zsh/Completion/_pluto" \
-  atpull"%atclone"
+	atclone"./pluto completion zsh > ${HOME}/.zsh/Completion/_pluto" \
+	atpull"%atclone"
 zinit light FairwindsOps/pluto
 # }}}
 # }}}
@@ -394,12 +393,12 @@ zinit light hashicorp/terraform-ls
 # }}}
 # terraform-docs/terraform-docs {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" has"terraform" \
-  mv"terraform-docs-*->terraform-docs"
+	mv"terraform-docs-*->terraform-docs"
 zinit light terraform-docs/terraform-docs
 # }}}
 # hashicorp/terraform-bundle {{{
-zinit ice wait'3' lucid as"program" has"go" id-as"hashicorp/terraform-bundle"\
-  atclone"go install ./tools/terraform-bundle" atpull"%atclone"
+zinit ice wait'3' lucid as"program" has"go" id-as"hashicorp/terraform-bundle" \
+	atclone"go install ./tools/terraform-bundle" atpull"%atclone"
 zinit light hashicorp/terraform
 # }}}
 # cappyzawa/tfswitch {{{
@@ -408,7 +407,7 @@ zinit light cappyzawa/tfswitch
 # }}}
 # im2nguyen/rover {{{
 zinit ice wait'1' lucid as"program" from"gh-r" \
-  mv"rover_* -> rover"
+	mv"rover_* -> rover"
 zinit light im2nguyen/rover
 # }}}
 # }}}
@@ -416,51 +415,51 @@ zinit light im2nguyen/rover
 
 # tonsky/FiraCode {{{
 zinit ice wait'1' lucid as"program" from:"gh-r" \
-  if'[[ "$(ostype)" == "darwin" ]]' \
-  atclone"cp ttf/FiraCode*.ttf ~/Library/Fonts"
+	if'[[ "$(ostype)" == "darwin" ]]' \
+	atclone"cp ttf/FiraCode*.ttf ~/Library/Fonts"
 zinit light tonsky/FiraCode
 # }}}
 
 # cloudfoundry/bosh-cli {{{
 zinit ice wait'2' lucid as"program" from"gh-r" \
-  mv"bosh-cli* -> bosh"
+	mv"bosh-cli* -> bosh"
 zinit light cloudfoundry/bosh-cli
 # }}}
 
 # minio/mc {{{
 zinit ice wait'3' lucid as"program" has"go" \
-  atclone"GO111MODULE=on go install ." atpull"%atclone"
+	atclone"GO111MODULE=on go install ." atpull"%atclone"
 zinit light minio/mc
 # }}}
 
 # bazelbuild/bazel {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  mv"bazel-*-*-x86_64*->bazel"
+	mv"bazel-*-*-x86_64*->bazel"
 zinit light bazelbuild/bazel
 # }}}
 
 # screwdriver-cd/sd-local {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  mv"sd-local_*->sd-local"
+	mv"sd-local_*->sd-local"
 zinit light screwdriver-cd/sd-local
 # }}}
 
 # denoland/deno_install {{{
 zinit ice wait'2' lucid as"program" \
-  atclone"curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=$ZPFX sh && deno completions zsh > ~/.zsh/Completion/_deno" atpull"%atclone"
+	atclone"curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=$ZPFX sh && deno completions zsh > ~/.zsh/Completion/_deno" atpull"%atclone"
 zinit light denoland/deno_install
 # }}}
 
 # protocolbuffers/protobuf {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  if'[[ "$(ostype)" == "darwin" ]]' \
-  bpick:"protoc-*-osx-*.zip" mv"bin/protoc->$ZPFX/bin/protoc"
+	if'[[ "$(ostype)" == "darwin" ]]' \
+	bpick:"protoc-*-osx-*.zip" mv"bin/protoc->$ZPFX/bin/protoc"
 zinit light protocolbuffers/protobuf
 # }}}
 
 # cue-lang/cue {{{
 zinit ice wait'2' lucid as"program" from:"gh-r" \
-  atclone"./cue completion zsh > ~/.zsh/Completion/_cue" atpull"%atclone"
+	atclone"./cue completion zsh > ~/.zsh/Completion/_cue" atpull"%atclone"
 zinit light cue-lang/cue
 # }}}
 
