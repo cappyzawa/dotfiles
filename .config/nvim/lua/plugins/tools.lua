99347602384e96b3430a2e0b739b67aa8bdeeca7
Shu Kutsuzawa <cappyzawa@gmail.com>
2023-01-28 21:32:35 +0900

    add git-messenger

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -6,4 +6,14 @@ return {
       { "tyru/open-browser.vim" },
     },
   },
+  {
+    "rhysd/git-messenger.vim",
+    cmd = "GitMessenger",
+    keys = { "<leader>gm", "<cmd>GitMessenger<CR>", desc = "Show Git Message" },
+    config = function()
+      vim.g.git_messenger_include_diff = "current"
+      vim.g.git_messenger_always_into_popup = true
+      vim.g.git_messenger_no_default_mappings = true
+    end,
+  },
 }
