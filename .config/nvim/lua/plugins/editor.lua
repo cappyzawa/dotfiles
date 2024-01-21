8ae33b08104d7f9b7eed7d281adafee60034cea8
cappyzawa <cappyzawa@gmail.com>
2024-01-21 20:46:44 +0900

    add dependencies

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -9,6 +9,21 @@ local icons = {
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+      {
+        "rcarriga/nvim-notify",
+        config = function()
+          require("telescope").load_extension("notify")
+        end,
+      },
+    },
     opts = function()
       local Util = require("lazyvim.util")
       local actions = require("telescope.actions")
