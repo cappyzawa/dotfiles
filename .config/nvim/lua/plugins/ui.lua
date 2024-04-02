6a2103aa73f81b5a381c994e4258d0812bd748f9
cappyzawa <cappyzawa@gmail.com>
2024-04-02 23:24:10 +0900

    disable dashboard

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -44,22 +44,26 @@ return {
       vim.treesitter.language.register("starlark", "tiltfile")
     end,
   },
+  --   {
+  --     "nvimdev/dashboard-nvim",
+  --     opts = function(_, opts)
+  --       local logo = [[
+  --  .d8888b. 8888888b.8888888888P888       888
+  -- d88P  Y88b888   Y88b     d88P 888   o   888
+  -- 888    888888    888    d88P  888  d8b  888
+  -- 888       888   d88P   d88P   888 d888b 888
+  -- 888       8888888P"   d88P    888d88888b888
+  -- 888    888888        d88P     88888P Y88888
+  -- Y88b  d88P888       d88P      8888P   Y8888
+  --  "Y8888P" 888      d8888888888888P     Y888
+  --   ]]
+  --       opts.config.header = vim.split(logo, "\n")
+  --       return opts
+  --     end,
+  --   },
   {
     "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      local logo = [[
- .d8888b. 8888888b.8888888888P888       888
-d88P  Y88b888   Y88b     d88P 888   o   888
-888    888888    888    d88P  888  d8b  888
-888       888   d88P   d88P   888 d888b 888
-888       8888888P"   d88P    888d88888b888
-888    888888        d88P     88888P Y88888
-Y88b  d88P888       d88P      8888P   Y8888
- "Y8888P" 888      d8888888888888P     Y888
-  ]]
-      opts.config.header = vim.split(logo, "\n")
-      return opts
-    end,
+    enabled = false,
   },
   {
     "folke/noice.nvim",
