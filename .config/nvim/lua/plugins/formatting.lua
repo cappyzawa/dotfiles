3e3bf81464aa5c402b377253695046fee7f84b47
cappyzawa <cappyzawa@gmail.com>
2025-05-16 06:00:52 +0900

    refactor(format): simplify conform.nvim configuration
    Replace function-based configuration with direct table structure for
    the conform.nvim plugin. This simplifies the code while maintaining
    the same functionality and formatting rules.

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,30 +1,28 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = function()
-      return {
-        default_format_opts = {
-          timeout_ms = 5000,
-        },
-        formatters_by_ft = {
-          css = { "prettier" },
-          go = { "goimports" },
-          html = { "prettier" },
-          javascript = { "prettier" },
-          json = { "prettier" },
-          -- json5 = { "prettier" },
-          lua = { "stylua" },
-          markdown = { "prettier" },
-          nix = { "nixfmt" },
-          rust = { "rustfmt" },
-          sh = { "shfmt" },
-          terraform = { "terraform_fmt" },
-          typescript = { "prettier" },
-          typescriptreact = { "prettier" },
-          yaml = { "yamlfmt" },
-          zsh = { "beautysh" },
-        },
-      }
-    end,
+    opts = {
+      default_format_opts = {
+        timeout_ms = 5000,
+      },
+      formatters_by_ft = {
+        css = { "prettier" },
+        go = { "goimports" },
+        html = { "prettier" },
+        javascript = { "prettier" },
+        json = { "prettier" },
+        -- json5 = { "prettier" },
+        lua = { "stylua" },
+        markdown = { "prettier" },
+        nix = { "nixfmt" },
+        rust = { "rustfmt" },
+        sh = { "shfmt" },
+        terraform = { "terraform_fmt" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        yaml = { "yamlfmt" },
+        zsh = { "beautysh" },
+      },
+    },
   },
 }
