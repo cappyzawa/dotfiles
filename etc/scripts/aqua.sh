d355bab4b781bfb9d3bc0bb4792207da65d5ef7e
cappyzawa <cappyzawa@gmail.com>
2026-01-08 01:40:48 +0900

    feat(aqua): add renovate support for installer versions
    Extract aqua-installer and aqua versions into variables with renovate
    comments. Add customManager to renovate config to enable automatic
    dependency updates for the aqua install script.
    
    Signed-off-by: cappyzawa <cappyzawa@gmail.com>

--- etc/scripts/aqua.sh
+++ etc/scripts/aqua.sh
@@ -7,4 +7,8 @@ fi
 
 echo "Installing aqua..."
 # Install specific version to avoid GitHub API rate limit
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v4.0.2/aqua-installer | bash -s -- -v v2.53.3
+# renovate: depName=aquaproj/aqua-installer
+AQUA_INSTALLER_VERSION="v4.0.4"
+# renovate: depName=aquaproj/aqua
+AQUA_VERSION="v2.56.1"
+curl -sSfL "https://raw.githubusercontent.com/aquaproj/aqua-installer/${AQUA_INSTALLER_VERSION}/aqua-installer" | bash -s -- -v "${AQUA_VERSION}"
